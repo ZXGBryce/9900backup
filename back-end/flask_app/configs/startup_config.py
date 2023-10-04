2061d82d18efe93c696f328868076033bd4c47c9
Xiangeng Zhao <zhaoxiangeng@gmail.com>
2023-10-04 23:12:44 +1100

    update

--- back-end/flask_app/configs/startup_config.py
+++ back-end/flask_app/configs/startup_config.py
@@ -17,7 +17,8 @@ class StartupConfig(BaseModel):
 def load_startup_config() -> StartupConfig:
     # 从环境变量获取当前部署环境，如果没有环境变量就使用DEV环境
     deploy_env = DeployEnv(os.environ.get("env", DeployEnv.DEV.value))
-    config_name = f"{deploy_env}.json"
+    print(deploy_env)
+    config_name = f"{deploy_env.value}.json"
     # 配置文件和当前文件在同一目录，获取当前目录
     config_dir = os.path.dirname(__file__)
     # 加载对应环境的配置
