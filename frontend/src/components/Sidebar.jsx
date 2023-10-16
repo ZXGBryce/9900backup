9e76a81d04df1145e88cd6f390543ac7a4f27596
ray <raymondf0123@gmail.com>
2023-10-16 18:38:54 +1100

    frontend bootstrap update

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,18 +1,10 @@
 import { Drawer, List, ListItem, ListItemIcon, ListItemText, Icon, ButtonBase, Box, Typography, } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
-const drawerWidth = 240; // Width of the sidebar
+import "../css/SideBar.css"
 
 
-const SideBar = () => {
+function SideBar () {
     const navigate = useNavigate()
-
-    const paths = {
-    'Dashboard': '/dashboard',
-    'Analysis History': '/analysis-history',
-    'New Analysis': '/newAnalysis',
-    'Profile': '/profile',
-    'Logout': '/logout'
-    }
 
     function logoutAction () {
         localStorage.clear();
@@ -23,49 +15,7 @@ const SideBar = () => {
     
 
 return (
-    <Drawer
-    variant="permanent"
-    style={{ 
-        width: drawerWidth,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        zIndex: 10,
-    }}
-    PaperProps={{ 
-        style: { 
-            width: drawerWidth,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center'
-        }
-    }}
-    >
-        <List>
-            {['Dashboard', 'Analysis History', 'New Analysis', 'Profile', 'Logout'].map((text, index) => (
-                <ButtonBase 
-                    key={text} 
-                    style={{ width: '100%', justifyContent: 'flex-start', marginBottom: '10px' }}
-                    onClick={() => {
-                        if (text === "Logout"){
-                            logoutAction()
-                        } else {
-                            navigate(paths[text])
-                        }
-                    }}
-                >
-                    <ListItem>
-                        <ListItemIcon>
-                            <Icon />
-                        </ListItemIcon>
-                        <ListItemText primary={
-                            <Typography variant="h6">{text}</Typography>
-                        } />
-                    </ListItem>
-                </ButtonBase>
-            ))}
-        </List>
-     </Drawer>
+    <div>Sidebar</div>
    )
 }
 
--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -1,7 +1,13 @@
+// import library and functions 
 import { Copyright } from '../helper';
 import { Button, TextField, Container, Paper, Typography, AppBar, Toolbar, Box} from '@mui/material';
 import { useState } from 'react';
-import SideBar from '../components/Sidebar.jsx'
+import callAPI from '../callAPI'
+
+// import components
+import SideBar from '../components/SideBar.jsx'
+import '../css/SideBar.css'
+
 
 function Admin (){
     const [file, setFile] = useState(null)
@@ -11,39 +17,12 @@ function Admin (){
 
     const token = localStorage.getItem('token')
 
-    ////// replace this code later with just callAPI from callAPI page
-    async function callAPI (method,token) {
-        console.log('API call starts with:', method);
-        const options = {
-        method: 'POST',
-        headers: {
-        Authorization: `Bearer ${token}`,
-        },
-        body: formData,
-        redirect: 'follow'
-    };
-    if (method === 'GET') {
-        delete options.body;
-    }
-
-    const response = await fetch(`http://127.0.0.1:5000/admin/upload_csv`, options);
-    const data = await response.json().then(
-
-    )
-    if (data.error) {
-        console.log(data.error);
-        throw new Error(data.error);
-    }
-    return data;
-    }
-    ////////////////////////////////////////////
-
     async function handleFileUpload(){
         if (!file) {
             console.error("No File Selected!")
             return
         }
-        callAPI('POST',token)
+        callAPI('POST',`admin/upload_csv`,token,formData)
         .then(response =>{
                 console.log(response)
                 console.log(response.data)
@@ -60,64 +39,9 @@ function Admin (){
     }
 
     return (
-        <Box display="flex" flexDirection="column" minHeight="100vh">
-        {/* Header */}
-        <AppBar position="static" >
-            <Toolbar>
-            <Typography variant="h6">
-                Glitch
-            </Typography>
-            </Toolbar>
-        </AppBar>
-
-        {/* Main Content + Sidebars wrapped */}
-        <Box flexGrow={1}> {/* Added margin top (mt) and margin bottom (mb) */}
-            <Box display="flex" flexDirection="row" height="100%">
-            {/* Left Sidebar */}
-            <Box width='240' bgcolor="grey.200">
-                <SideBar />
-            </Box>
-
-            {/* Central Container */}
-            <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Paper elevation={3} style={{ padding: '2rem', height: '300px', width: '500px' }}>
-                    <Typography variant="h5" align="center" gutterBottom>
-                                    CSV File Upload
-                                </Typography>
-                                <TextField 
-                                    label="Select CSV File" 
-                                    type="file" 
-                                    variant="outlined" 
-                                    fullWidth 
-                                    style={{ marginBottom: '1rem', marginTop:'50px'}}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    inputProps={{
-                                        accept: ".csv"
-                                    }}
-                                    onChange={(e) => setFile(e.target.files[0])}
-                                />
-                                <Button variant="contained" color="primary" type="submit" fullWidth onClick={handleFileUpload}>
-                                    Upload
-                                </Button>
-                </Paper>
-            </Container>
-
-            {/* Right Empty Sidebar */}
-            <Box width='240' bgcolor="grey.200">
-                {/* Empty for now */}
-            </Box>
-            </Box>
-        </Box>
-
-        {/* Footer */}
-        <Box height="50px" bgcolor="grey.300" display="flex" justifyContent="center" alignItems="center">
-            <Typography>
-            Footer Content Here
-            </Typography>
-        </Box>
-        </Box>
+        <div>
+            <SideBar/>
+        </div>
 
         
     );
