c60c6595a052454503a3ee98a855b98cb2f0293d
Dongxuan Li <452057440@qq.com>
2023-10-22 22:01:40 +1100

    Write a draft for query data

--- backend/flask_app/libs/data_access.py
+++ backend/flask_app/libs/data_access.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
 from peewee import Database
-
+import pandas as pd
 from flask_app.models.user import AuthUserTab
+from flask_app.models.dataset import DataSetTab
 
 
 class DataAccess:
@@ -38,3 +39,67 @@ class DataAccess:
         """ 更新版本号 """
         user.jwt_version += 1
         user.save()
+
+    def getdatasetvalue(self, entities: list, indicators: list, framework: str, sasb: str, region: list, sector: list, environment: str, social: str, governance: str):
+        """ gets all the filters"""
+        company_name_filter = entities
+        indicators_filter = indicators
+        framework_filter = framework
+        sasb_filter = sasb
+        region_filter = region
+        sector_filter = sector
+        environment_filter = environment
+        social_filter = social
+        governance_filter = governance
+        query = DataSetTab.select()
+        #filter out those column selected 筛选掉有项的
+        if company_name_filter:
+            query = query.where(DataSetTab.company_name << company_name_filter)
+        if indicators_filter:
+            query = query.where(DataSetTab.indicator_name << indicators_filter)
+        if framework_filter != "None":
+             query = query.where(DataSetTab.framework == framework_filter)
+        if sasb_filter != 'None':
+            if sasb_filter == 'Yes':
+                query = query.where(DataSetTab.sasb_materiality == 'Yes')
+            else:
+                query = query.where(DataSetTab.sasb_materiality == 'No')
+        if region_filter:
+            query = query.where(DataSetTab.region << region_filter)
+        if sector_filter:
+            query = query.where(DataSetTab.sector << sector_filter)
+        if environment_filter != 'None':
+            if environment_filter == 'True':
+                query = query.where(DataSetTab.environment == True)
+            elif environment_filter == 'False':
+                query = query.where(DataSetTab.environment == False)
+        if social_filter != 'None':
+            if social_filter == 'True':
+                query = query.where(DataSetTab.social == True)
+            elif social_filter == 'False':
+                query = query.where(DataSetTab.social == False)
+        if governance_filter != 'None':
+            if governance_filter == 'True':
+                query = query.where(DataSetTab.governance == True)
+            elif governance_filter == 'False':
+                query = query.where(DataSetTab.governance == False)
+        # 将查询结果转换为list change query into list
+        results = list(query)
+        data = [
+            {
+                'company_name': row.company_name,
+                'indicator_name': row.indicator_name,
+                'indicator_value': row.indicator_value,
+                'sasb_materiality': row.sasb_materiality,
+                'region': row.region,
+                'sector': row.sector,
+                'environment': row.environment,
+                'social': row.social,
+                'governance': row.governance,
+                'timestamp': row.timestamp,
+                'data_source': row.data_source,
+            }
+            for row in results
+        ]
+        # 返回list return list
+        return data
---
+++ backend/flask_app/server/api/indi.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, Request, jsonify, make_response
+from pydantic import BaseModel
+import json
+from flask_app.server.consts.response_format import Response, Code
+from flask_app.server.utils.request_parser import handle_with_pydantic
+from flask_app.libs.registry import DependencyRegistry as dep
+
+# 定义blueprint可以把一类的接口放在一起管理，不用像quickstart中都一股脑写在app上
+# url_prefix是/auth 访问下面的sign_up接口时地址就是这样的http://localhost:8000/auth/sign_up
+indi_blueprint = Blueprint('analysis', __name__, url_prefix="/analysis")
+
+class IndiRequest(BaseModel):
+    """定义注册接口的请求参数"""
+    entities: list
+    indicators: list
+    framework: str
+    sasb: str
+    region: list
+    sector: list
+    environment: str
+    social: str
+    governance: str
+
+class IndiResponse(BaseModel):
+    query: list
+@indi_blueprint.get('/indicators')  # 在blueprint注册请求
+@handle_with_pydantic(IndiRequest)
+def custom_indi(request: IndiRequest) -> Response:
+    data_query = dep.data_access.getdatasetvalue(request.entities,request.indicators,request.framework,request.sasb,request.region,request.sector,request.environment,request.social,request.governance)  # 注意这里也使用request.username，因为用户可能输入邮箱作为用户名登录
+    # 返回 JSON 数据作为响应
+    if data_query == []:
+        return Response(data=IndiResponse(query=data_query), code=Code.REQ_JSON_INVALID)
+    return Response(data=IndiResponse(query=data_query), code=Code.OK)
