92c9f297c3b455738efad2583000f951ff32a3f4
ray <raymondf0123@gmail.com>
2023-10-25 01:05:58 +1100

    new framework selection

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,7 +1,11 @@
 import { Copyright } from '../helper';
 import { BrowserRouter, Routes, Link, Route, useNavigate, useLocation } from 'react-router-dom';
-import Button from '@mui/material/Button';
 import callAPI from '../callAPI';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import "../css/pages/Dashboard.css"
+import "../css/Site.css"
 
 const Dashboard = (props) => {
     const navigate = useNavigate();
@@ -44,44 +48,22 @@ const Dashboard = (props) => {
     }
     const logoutAction = async () => {
       localStorage.clear();
-      // localStorage.removeItem('token');
-      // localStorage.removeItem('email');
-      props.setToken(null);
       navigate('/');
     }
     
     return (
-      <div style={{height:'100vh'}}>
-        <div style={{ display: 'flex', alignItems: 'center', height:'10%',justifyContent: 'space-between', width: '100%'}}>
-          <div style={{marginLeft: '5%', display: 'flex ', alignItems: 'center'}}><h1>Glitch</h1></div>
-          <Button variant="contained" color="primary" onClick={handleAdminOnClick} style={{ marginRight: '5%' }}>
-            Admin
-          </Button>
-        </div>
-        {/* <hr /> */}
-        
-        {/* Bottom half */}
-        <div style={{ display: 'flex', alignItems: 'center', height:'80%', justifyContent: 'center'}}>
-          <div style={{ display: 'flex', flexDirection:'column', alignItems: 'center', width: '10%', height: '100%', border: '1px solid black'}}>
-            <div className='unclickableDiv'>Dashboard</div>
-            <div className='clickableDiv'>Analysis History</div>
-            <div className='clickableDiv' onClick={() => navigate('/newAnalysis')}>New Analysis</div>
-            <div className='clickableDiv'>Profile</div>
-            <div className='clickableDiv'onClick={logoutAction}>Logout</div>
+      <div className='site-struct'>
+        <Header/>
+        <div className='main-container'>
+          <div className='container-block'>
+            <h1>Block 1</h1>
           </div>
-          <div style={{ display: 'flex ', flexDirection:'column', alignItems: 'center', justifyContent: 'center', width: '80%',height: '100%', border: '1px solid black'}}>
-            <div style={{width: '100%', height: '10%', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid black'}}><h2>Dashboard</h2></div>
-            <div style={{width: '100%', height: '45%', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid black'}}>User Information</div>
-            <div style={{width: '100%', height: '45%', display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid black'}}>News and Update</div>
-          </div>
-          <div style={{ display: 'flex ', alignItems: 'center', width: '10%',height: '100%', border: '1px solid black'}}>
+          <hr />
+          <div className='container-block'>
+            <h1>Block 2</h1>
           </div>
         </div>
-        
-        <div className="App">
-          
-        </div>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+        <Footer/>
       </div>
     )
   }
