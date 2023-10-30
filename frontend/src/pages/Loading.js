1f53f90204dd00c0b9ad37eb8514cbe4c0cab75b
Pat-2022 <969329246@qq.com>
2023-10-31 01:54:09 +1100

    Add loading effect

--- frontend/src/pages/Loading.js
+++ frontend/src/pages/Loading.js
@@ -3,33 +3,37 @@ import '../css/Loading.css';
 
 const Loading = () => {
     return (
-        <div className="loading-body">
-            <div class="boxes">
-                <div class="box">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+        <div className="loading-page">
+            <div className="loading-body">
+                <div class="boxes">
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
-                <div class="box">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div class="box">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div class="box">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
+                
             </div>
+            <div class="loading-text">LOADING</div>
         </div>
     );
 }
