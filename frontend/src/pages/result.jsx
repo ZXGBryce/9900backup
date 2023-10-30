696c1a1cf91a768983e56b65d97eb321222fb1b2
Pat-2022 <969329246@qq.com>
2023-10-30 19:09:30 +1100

    Pat

--- frontend/src/pages/result.jsx
+++ frontend/src/pages/result.jsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { Box, Checkbox, FormControlLabel, Typography, Button } from '@mui/material'
 
 const Result= (props) => {
     return (
@@ -7,8 +8,9 @@ const Result= (props) => {
           <Header/>
           <div className='main-container'>
             <div className='container-block'>
-              <h1>Block 1</h1>
+              <h1>Analysis Result</h1>
             </div>
+            <Button style={{ marginTop:'10px', backgroundColor:'white'}} onClick={() => console.log('Save Result')}>Save Result</Button>
           </div>
           <Footer/>
         </div>
