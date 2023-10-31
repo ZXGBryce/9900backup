3ee242e32b3c71a46cd640c257f908c21a33230b
Pat-2022 <969329246@qq.com>
2023-11-01 06:19:09 +1100

    add News

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -36,10 +36,20 @@ const Dashboard = (props) => {
         <div className='main-container'>
           <div className='container-block'>
             <h1>Weekly News</h1>
+            <div className='news-bloclk'>
+                <p><h5>1.Global Shift towards ESG:</h5> The recent UN conference highlighted the importance of ESG metrics in driving sustainable growth in the financial sector. FinTech companies are now in a better position to leverage these metrics for ethical investments.</p>
+                <p><h5>2.Paris Agreement's New Targets:</h5> The Paris Agreement's updated goals for 2023 emphasize the significance of transparent ESG reporting. FinTech industries worldwide are urged to align their financial products accordingly.</p>
+                <p><h5>3.TCFD Recommendations:</h5> The Task Force on Climate-related Financial Disclosures (TCFD) has recently released a new set of guidelines on integrating climate risks and opportunities in financial decision-making.</p>
+            </div>
           </div>
           <hr />
           <div className='container-block'>
             <h1>System Update</h1>
+            <div className='news-bloclk'>
+              <p><h5>Version 2.0.1 - Released on [03 Nov 2023]</h5></p>
+              <p><h5>1.Account Management Enhancements:</h5><b>·</b> Streamlined user registration process with reduced steps.</p>
+              <p><h5>2.Framework Selection:</h5><b>·</b> Added three ESG frameworks to the list.<br/><b>·</b> Enhanced UI for easier framework browsing.</p>
+            </div>
           </div>
         </div>
         <Footer/>
