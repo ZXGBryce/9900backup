696448417cc632c52d5f0265221e7df76dd90940
Xiangeng Zhao <zhaoxiangeng@gmail.com>
2023-11-16 19:53:47 +1100

    update

--- backend/flask_app/server/utils/chatgpt.py
+++ backend/flask_app/server/utils/chatgpt.py
@@ -11,11 +11,11 @@ def generate_description(esg_data1, esg_data2):
         messages=[
             {
                 "role": "user",
-                "content": f"Generate a simple conclusion at least 200 words based on the following ESG data: {esg_data1}, {esg_data2}",
+                "content": f"Generate a simple conclusion no more than 150 words based on the following ESG data: {esg_data1}, {esg_data2}",
             }
         ],
-        model="gpt-3.5-turbo",
-        max_tokens=1000
+        model="gpt-3.5-turbo-1106",
+        max_tokens=600
     )
 
     return chat_completion.choices[0].message.content
